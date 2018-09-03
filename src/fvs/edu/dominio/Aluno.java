1c36201b92a5dd723db261028f8d27dbe1ba2c7e
unknown <alefpereira@gmail.com>
2018-09-03 20:11:25 -0300

    Aulajpa1

--- src/fvs/edu/dominio/Aluno.java
+++ src/fvs/edu/dominio/Aluno.java
@@ -2,167 +2,124 @@ package fvs.edu.dominio;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Aluno implements Serializable {
 
-private static final long serialVersionUID = 1L;
-
-private Integer Id;
-private String nome;
-private String cpf;
-private String rg;
-private Integer matricula;
-
-
-
-public Aluno(Integer id, String nome, String cpf, String rg, Integer matricula) {
-	super();
-	Id = id;
-	this.nome = nome;
-	this.cpf = cpf;
-	this.rg = rg;
-	this.matricula = matricula;
-}
-
-
-
-
-
-public Integer getId() {
-	return Id;
-}
-
-
-
-
-
-public void setId(Integer id) {
-	Id = id;
-}
-
-
-
-
-
-public String getNome() {
-	return nome;
-}
-
-
-
-
-
-public void setNome(String nome) {
-	this.nome = nome;
-}
-
-
-
-
-
-public String getCpf() {
-	return cpf;
-}
-
-
-
-
-
-public void setCpf(String cpf) {
-	this.cpf = cpf;
-}
-
-
-
-
-
-public String getRg() {
-	return rg;
-}
-
-
-
-
-
-public void setRg(String rg) {
-	this.rg = rg;
-}
-
-
-
-
-
-public Integer getMatricula() {
-	return matricula;
-}
-
-
-
-
-
-public void setMatricula(Integer matricula) {
-	this.matricula = matricula;
-}
-
-
-
-
-
-@Override
-public String toString() {
-	return "Aluno [Id=" + Id + ", nome=" + nome + ", cpf=" + cpf + ", rg=" + rg + ", matricula=" + matricula + "]";
-}
-
-
-
-
-
-@Override
-public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((Id == null) ? 0 : Id.hashCode());
-	result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
-	result = prime * result + ((matricula == null) ? 0 : matricula.hashCode());
-	result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-	result = prime * result + ((rg == null) ? 0 : rg.hashCode());
-	return result;
-}
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private Integer Id;
+	private String nome;
+	private String cpf;
+	private String rg;
+	private Integer matricula;
+
+	public Aluno(Integer id, String nome, String cpf, String rg, Integer matricula) {
+		super();
+		Id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.rg = rg;
+		this.matricula = matricula;
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public Integer getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(Integer matricula) {
+		this.matricula = matricula;
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [Id=" + Id + ", nome=" + nome + ", cpf=" + cpf + ", rg=" + rg + ", matricula=" + matricula + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((matricula == null) ? 0 : matricula.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((rg == null) ? 0 : rg.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (matricula == null) {
+			if (other.matricula != null)
+				return false;
+		} else if (!matricula.equals(other.matricula))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (rg == null) {
+			if (other.rg != null)
+				return false;
+		} else if (!rg.equals(other.rg))
+			return false;
 		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Aluno other = (Aluno) obj;
-	if (Id == null) {
-		if (other.Id != null)
-			return false;
-	} else if (!Id.equals(other.Id))
-		return false;
-	if (cpf == null) {
-		if (other.cpf != null)
-			return false;
-	} else if (!cpf.equals(other.cpf))
-		return false;
-	if (matricula == null) {
-		if (other.matricula != null)
-			return false;
-	} else if (!matricula.equals(other.matricula))
-		return false;
-	if (nome == null) {
-		if (other.nome != null)
-			return false;
-	} else if (!nome.equals(other.nome))
-		return false;
-	if (rg == null) {
-		if (other.rg != null)
-			return false;
-	} else if (!rg.equals(other.rg))
-		return false;
-	return true;
-}
-
+	}
 
 }
