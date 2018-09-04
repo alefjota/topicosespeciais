37a6464e963f38ffbcef2855210c7fccb9ac7bc1
= <=>
2018-09-04 20:07:33 -0300

    AULA JPA2

--- src/fvs/edu/aplicacao/Principal.java
+++ src/fvs/edu/aplicacao/Principal.java
@@ -4,13 +4,23 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import fvs.edu.dominio.Aluno;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("topicos-fvs");
 		
 		EntityManager em = emf.createEntityManager();
-
+		Aluno aluno = em.find(Aluno.class, 1);
+		System.out.println(aluno);
+		
+		//em.getTransaction().begin();
+		//Aluno aluno1 = new Aluno ( null,"Alef" , "159" , "552" , 859);
+		//em.persist(aluno1);
+		//em.getTransaction().commit();
+		
+		System.out.println("Salvo com Sucesso");
 	}
 
 }
